cb936a7ba1a66cdbeadff33ef521f80f7f7d8e5e
stephen7-kojo-awoninyam <107375548+stephen7-kojo-awoninyam@users.noreply.github.com>
2023-05-04 22:24:48 -0400

    testing my knowledge on strings

---
+++ string2.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.length() + B.length());
+        
+        int result = A.compareTo(B);
+        
+        if(result<0){
+            
+            System.out.println("No");
+        } else{
+            
+            System.out.println("Yes");
+        }
+        
+        String FirstCharOfA = A.substring(0,1).toUpperCase();
+        
+        String RestCharOfA = A.substring(1);
+        
+        String CharOfA = FirstCharOfA + RestCharOfA;
+        
+         String FirstCharOfB = B.substring(0,1).toUpperCase();
+         
+        String RestCharOfB = B.substring(1);
+        
+        String CharOfB = FirstCharOfB + RestCharOfB;
+        System.out.println(CharOfA +" "+CharOfB);
+    }
+}
+
+
+
