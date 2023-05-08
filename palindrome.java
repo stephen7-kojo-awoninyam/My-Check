1b4a85cb2f9d38e5d93112c336d808fed0c9f760
stephen7-kojo-awoninyam <107375548+stephen7-kojo-awoninyam@users.noreply.github.com>
2023-05-08 11:46:59 -0400

    Palindrome

---
+++ palindrome.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT.
+     */
+        
+            
+            StringBuilder N = new StringBuilder(A);
+            
+            N.reverse();
+            
+            StringBuilder M = new StringBuilder(A);
+            
+            //System.out.println(N);
+            
+            if(N.toString().equals(M.toString())){
+                
+                System.out.println("Yes");
+                
+            }else{
+                
+                System.out.println("No");
+            }
+        
+        
+    }
+}
+
+
+
