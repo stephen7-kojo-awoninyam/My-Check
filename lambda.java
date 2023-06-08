e01d71062410e4fb8f68b4c6ffdcf4157fb1e035
stephen7-kojo-awoninyam <107375548+stephen7-kojo-awoninyam@users.noreply.github.com>
2023-06-07 20:30:15 -0400

    lambda expression

---
+++ lambda.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.*;
+interface PerformOperation {
+ boolean check(int a);
+}
+class MyMath {
+ public static boolean checker(PerformOperation p, int num) {
+  return p.check(num);
+ }
+   // Write your code here
+ public PerformOperation isOdd() {
+  return (a)->{
+     if(a%2 != 0){
+         return true;
+         }else{
+             
+             return false;
+         }
+ };
+ }
+ public PerformOperation isPrime() {
+ return (a)->{
+     
+        if (a <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(a); i++) {
+            if (a % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+ }
+ 
+ public PerformOperation isPalindrome() {
+   return (a)->{
+           String str = String.valueOf(a);
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+   };
+
+        
+ } 
+ }
+public class Solution {
+
+ public static void main(String[] args) throws IOException {
+  MyMath ob = new MyMath();
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  int T = Integer.parseInt(br.readLine());
+  PerformOperation op;
+  boolean ret = false;
+  String ans = null;
+  while (T--> 0) {
+   String s = br.readLine().trim();
+   StringTokenizer st = new StringTokenizer(s);
+   int ch = Integer.parseInt(st.nextToken());
+   int num = Integer.parseInt(st.nextToken());
+   if (ch == 1) {
+    op = ob.isOdd();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "ODD" : "EVEN";
+   } else if (ch == 2) {
+    op = ob.isPrime();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "PRIME" : "COMPOSITE";
+   } else if (ch == 3) {
+    op = ob.isPalindrome();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+
+   }
+   System.out.println(ans);
+  }
+ }
+}
