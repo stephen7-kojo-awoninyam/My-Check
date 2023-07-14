81e1559a2212c19f0c411ff3951e28787dbb6183
stephen7-kojo-awoninyam <107375548+stephen7-kojo-awoninyam@users.noreply.github.com>
2023-07-14 14:15:55 -0400

    singleton pattern using java

---
+++ singleton.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+class Singleton{
+    
+    private static Singleton instance;
+    
+    private Singleton(){
+        
+    }
+    
+    public String str;
+   public  static Singleton getSingleInstance(){
+        
+       if(instance == null){
+           
+           instance = new Singleton();
+       }
+        return instance;
+    }
+
+}
